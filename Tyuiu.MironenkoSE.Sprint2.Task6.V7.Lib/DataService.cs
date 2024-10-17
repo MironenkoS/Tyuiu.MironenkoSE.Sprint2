e7964aa0561c0b3427ad4950114e74d2e8ba366c
664 <stephencurrytopone@gmail.com>
2024-10-17 19:52:16 +0500

    Task 6

--- Tyuiu.MironenkoSE.Sprint2.Task6.V7.Lib/DataService.cs
+++ Tyuiu.MironenkoSE.Sprint2.Task6.V7.Lib/DataService.cs
@@ -7,20 +7,21 @@ namespace Tyuiu.MironenkoSE.Sprint2.Task6.V7.Lib
         public string FindMonthName(int startYear, int n)
         {
             int startyear = n % 12;
+            
             switch (startyear)
             {
                 case 1: return "Январь";
-                case 2: return "Январь";
-                case 3: return "Январь";
-                case 4: return "Январь";
-                case 5: return "Январь";
-                case 6: return "Январь";
-                case 7: return "Январь";
-                case 8: return "Январь";
-                case 9: return "Январь";
-                case 10: return "Январь";
-                case 11: return "Январь";
-                case 12: return "Январь";
+                case 2: return "Февраль";
+                case 3: return "Март";
+                case 4: return "Апель";
+                case 5: return "Май";
+                case 6: return "Июнь";
+                case 7: return "Июль";
+                case 8: return "Август";
+                case 9: return "Сентябрь";
+                case 10: return "Октябрь";
+                case 11: return "Ноябрь";
+                case 12: return "Декабрь";
                 default: throw new ArgumentException($"Данные не верны. Значение {startyear}");
             }
         }
